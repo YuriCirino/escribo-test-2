6c7d01ca2dfbbff006bcb34c4c78f2fc79774045
Yuri Cirino <yuricirino@outlook.com>
2023-11-29 23:58:11 -0300

    added routes

--- src/server.ts
+++ src/server.ts
@@ -2,17 +2,26 @@ import express from 'express';
 import { prisma } from './lib/prisma';
 import userRouter from './routes/user'
 import bodyParser from 'body-parser';
+import formatDate from './utils'
 
 const app = express()
 app.use(bodyParser.json())
 app.use(userRouter)
+console.log(formatDate(new Date()))
 
-app.get('/', async (request, response) => {
+app.get('/deleteall', async (request, response) => {
     // const userCreated = await prisma.user.create({data:{name:"John Doe",email:"jd@email.com",password:"senhafraca2"}})
-    const allUsers = await prisma.user.findMany()
-    return response.send({message:"Hello World"
-    ,allUsers})
+    await prisma.phoneNumbers.deleteMany({})
+    await prisma.user.deleteMany({})
+    return response.send({
+        message: "Deleted All"
+    })
 })
+app.get('/users', async (request, response) => {
+    const allUsers = await prisma.user.findMany({})
+    response.send(allUsers)
+})
+
 
 app.listen(3000, () => {
     console.log('Server running on http://localhost:' + 3000)
